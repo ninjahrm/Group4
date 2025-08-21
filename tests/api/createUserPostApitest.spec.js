d93d2e74dc0f37a095cdd39b15d149d5d3b8fed9
sashi-ninjahrmgrp4 <sashi.ninjahrmgrp4@gmail.com>
2025-08-20 20:42:20 -0700

    adding user test for api

---
+++ tests/api/createUserPostApitest.spec.js
@@ -0,0 +1,42 @@
+import { test, expect,request } from '@playwright/test'
+import { userPostApiData } from '../testdata/userPostApiData';
+import { queryDB } from '../testdata/dbUtils';
+test.describe('User API tests', () => {
+
+let apiContext;
+console.log(userPostApiData.URLs.baseurl);
+test.beforeAll(async ({ playwright }) => {
+apiContext = await request.newContext({
+baseURL:userPostApiData.URLs.baseurl,
+extraHTTPHeaders:userPostApiData.extraHTTPHeaders // Replace with actual base URL
+
+});
+});
+//const { queryDB } = require('./dbUtils');
+test('Create new user', async () => {
+
+console.log(userPostApiData.URLs.endpoint);
+const response = await apiContext.post(userPostApiData.URLs.endpoint,
+    {
+        data: userPostApiData.userCreationData
+    }
+);
+
+expect(response.ok()).toBeTruthy(); // checks 2xx status
+const responseBody = await response.json();
+console.log(responseBody);
+
+expect(responseBody.empName).toBe(userPostApiData.userCreationData.empName);
+
+const dbData = await queryDB('SELECT * FROM employee where emp_name= ?',[responseBody.empName]);
+console.log(dbData[0].emp_id);
+console.log(dbData[0].emp_name);
+
+});
+
+test.afterAll(async () => {
+await apiContext.dispose();
+});
+});
+
+
