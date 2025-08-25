d140db71be7f0236653b019ae4a72fad571da93b
sashi-ninjahrmgrp4 <sashi.ninjahrmgrp4@gmail.com>
2025-08-24 20:22:20 -0700

    added more test cases to post and delete

--- testdata/apitestdata/UserData/userPostApiData.js
+++ testdata/apitestdata/UserData/userPostApiData.js
@@ -5,10 +5,18 @@ export const userPostApiData ={
         designation: faker.person.jobTitle(),
         dob: "02/01/2000",
         email: faker.internet.email(),
-        empName: faker.person.fullName(),
+        empName: faker.person.fullName()+ Math.floor(Math.random() * 10),
         experience: faker.number.int({ min: 1, max: 20 }),
         mobileNo: faker.number.int({ min: 1000000000, max: 9999999999 }).toString(),
         role: faker.person.jobType(),
+        username: faker.internet.username()+Math.floor(Math.random() * 100),
+        password : faker.internet.password(8,true,/[A-Z]/,[1,'@','#','$','%','&','*'] )
+        
+    },
+    userCreationDatawithMandatoryfields: {
+	    empName: faker.person.fullName(),
+        email: faker.internet.email(),
+        mobileNo: faker.number.int({ min: 1000000000, max: 9999999999 }).toString(),
         username: faker.internet.username(),
         password : faker.internet.password(8,true,/[A-Z]/,[1,'@','#','$','%','&','*'] )
     },
@@ -24,5 +32,22 @@ export const userPostApiData ={
         'Content-Type': "application/json",
         Authorization: "Basic " + Buffer.from("rmgyantra:rmgy@9999").toString("base64")
     }
-
 };
+
+export function generateUniqueUserData(overrides = {}) {
+  const uniqueId = `${Date.now()}_${Math.floor(Math.random() * 100000)}`;
+
+  return {
+    username: `user_${uniqueId}`,
+    email: `user_${uniqueId}@test.com`,
+    empName: faker.person.fullName()+ Math.floor(Math.random() * 10),
+    mobileNo: faker.number.int({ min: 1000000000, max: 9999999999 }).toString(),
+    dob: '02/01/2000',
+    experience: 2,
+    designation: faker.person.jobTitle(),
+    password: faker.internet.password(),
+    role: faker.person.jobType(),
+    department: faker.commerce.department(),
+    ...overrides, // allows you to override fields per test
+  };
+}
--- tests/api/deleteUserApitest.spec.js
+++ tests/api/deleteUserApitest.spec.js
@@ -1,6 +1,8 @@
 import { test, expect, request } from '@playwright/test';
 import { userPostApiData } from '../../testdata/apitestdata/UserData/userPostApiData';
 import { queryDB } from '../../utils/dbUtils';
+let userId1;
+test.describe.serial('User flow', () => {
 test.describe('User API tests',() => {
 
 let apiContext;
@@ -27,6 +29,7 @@ console.log(responseBody);
 
 //Delete the user created above
     const userId = responseBody.empId;
+    userId1 = responseBody.empId;
     console.log(userId);
     const delResponse = await apiContext.delete(userPostApiData.URLs.endpointdelete,{
 	params: { userId } });
@@ -35,10 +38,27 @@ console.log(responseBody);
   // Parse JSON response
 
   const body = await delResponse.text();
+  console.log('delResponse-status: ', delResponse.status());
   console.log('User deleted successfully');
   console.log(body);
 
  
+});
+
+test('Try deleting the same user again', async () => {
+  console.log('Attempting to delete same user again:', userId1);
+  console.log(userId1);
+    const delResponse1 = await apiContext.delete(userPostApiData.URLs.endpointdelete,{
+	params: { userId1 } });
+    expect(delResponse1.ok()).toBeFalsy();
+
+  // Parse JSON response
+
+  const body = await delResponse1.text();
+  console.log('delResponse-status: ', delResponse1.status());
+  console.log('User deleted not success');
+  console.log(body);
+
 });
 
 
@@ -46,4 +66,6 @@ console.log(responseBody);
 test.afterAll(async () => {
 await apiContext.dispose();
 });
+});
+
 });
